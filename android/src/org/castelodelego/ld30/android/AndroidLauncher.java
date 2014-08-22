8017e70c79f7371e9811411231a187e9e014829b
Claus Aranha <caranha@gmail.com>
2014-08-23 00:32:29 +0900

    Initial commit

---
+++ android/src/org/castelodelego/ld30/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.castelodelego.ld30.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.castelodelego.ld30.LD30Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LD30Game(), config);
+	}
+}
---
+++ desktop/src/org/castelodelego/ld30/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.castelodelego.ld30.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.castelodelego.ld30.LD30Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD30Game(), config);
+	}
+}
